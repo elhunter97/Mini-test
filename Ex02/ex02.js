b93bce1b995856c69f23a782f6a8ad5669a7fc27
elhunter97 <hanbaongoc14091271@gmail.com>
2024-04-24 11:43:59 +0700

    update ham bai 2

--- Ex02/ex02.js
+++ Ex02/ex02.js
@@ -9,6 +9,24 @@ function rank(mark){
     return r;
 }
 
+function countStudentRank(arr){
+    let cntA = 0;
+    let cntB = 0;
+    let cntC = 0;
+    let newArr= [];
+    for (let i = 0; i < arr.length; i++) {
+        if(rank(arr[i])==='A') cntA++;
+        if(rank(arr[i])==='B') cntB++;
+        if(rank(arr[i])==='C') cntC++;
+    }
+    newArr.push(cntA);
+    newArr.push(cntB);
+    newArr.push(cntC);
+
+    return newArr;
+}
+
+
 function avr(arr){
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -28,19 +46,11 @@ for (let i = 0; i < length; i++) {
         arr[i] = +prompt('Student '+i);
     }while(!Number.isInteger(arr[i]) || arr[i]<0 || arr[i]>10);
 }
+// tinh so luong hs tung hang
+console.log('Number of Student rank A = '+countStudentRank(arr)[0]);
+console.log('Number of Student rank B = '+countStudentRank(arr)[1]);
+console.log('Number of Student rank C = '+countStudentRank(arr)[2]);
 
-let cntA = 0;
-let cntB = 0;
-let cntC = 0;
-for(let i = 0;i<length;i++){
-    if(rank(arr[i])==='A') cntA++;
-    if(rank(arr[i])==='B') cntB++;
-    if(rank(arr[i])==='C') cntC++;
-}
-// Dem moi hang co bao nhieu hoc sinh
-console.log('Rank A: '+cntA+' students');
-console.log('Rank B: '+cntB+' students');
-console.log('Rank C: '+cntC+' students');
 
 // tinh diem trung binh
 let avrMark = avr(arr);
